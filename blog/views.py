2cf603dc564fe3e0607f3005780332690d2e01d8
Fabian Lien <fabian.lien@gmail.com>
2022-05-06 11:56:04 +0000

    add alert messages and unset debug

--- blog/views.py
+++ blog/views.py
@@ -71,10 +71,10 @@ class PostLike(View):
 
     def post(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
-        
+
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-        
+
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
